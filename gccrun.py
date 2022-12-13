76b9a79bb0c42d4834e86bd418a891973b37be66
Everton Jr <evertonse.junior@gmail.com>
2022-12-13 08:49:14 -0300

    gcc run init

---
+++ gccrun.py
@@ -0,0 +1,168 @@
+from dataclasses import dataclass
+import os
+import subprocess
+import tomllib as toml
+import pathlib
+from collections import defaultdict
+  
+
+
+#>>========================================================================================================
+#>>=============================================CONFIG PROEJCT=============================================
+# WARNING:
+# 	require either tomllib installed or python 3.11 (tomllib became part of the standard library for python 3.11)
+# PLEASE:
+# 	Create a toml files as your project config
+# 	Define **ALL** variables listed in the class Project bellow, don't need to be in order
+# 	Then populate with the desired values using the type indicated below
+@dataclass
+class Project():
+	name :str 
+	
+	compiler :str		# can optionally use full path
+	language :str  	# ignored for now
+	version  :str
+	binpath 	 :str # directory for binaries, if
+	#-O
+	optimize :str
+	srcfiles 		:list[str]
+	# -D
+	defines 		:list[str]
+	# -I
+	includedirs	:list[str]
+	# -L
+	libdirs 		:list[str]
+	# -l
+	libfiles 		:list[str]
+	#-W
+	warnings 		:list[str]
+#<<=========================================================================================================
+#<<=========================================================================================================
+
+	def __init__(self,toml_dict) -> None:
+		self.__dict__ = toml_dict
+		self.binpath = pathlib.Path(pathlib.PurePath('./'+ toml_dict['binpath']))
+		self.binpath.mkdir(parents=True,exist_ok=True)
+		self.add_src_files()
+
+	# // TODO(Everton): "ADD Regex to this"
+	def add_src_files(self):
+		for srcfile in list(self.srcfiles):
+			if srcfile.endswith("**"):
+				
+				srcdir = srcfile[:len(srcfile)-2]
+				for file in os.listdir(srcdir):
+					if file.endswith(".cpp") or file.endswith(".c"):
+						self.srcfiles.append(os.path.join(srcdir, file))
+				
+				self.srcfiles.remove(srcfile)
+
+	def executable_path(self):
+		return str(pathlib.PurePath(self.binpath,self.name))
+
+# cdir = os.getcwd() # it will return current working directory
+# print("Previous_dir",cdir)
+
+# # Previous_dir C:\Users\..\Desktop\python
+# os.chdir('./..') #chdir used for change direcotry
+# print("Current_dir",cdir)
+
+import sys
+
+__filename__,_ =  __file__[__file__.rindex('\\')+1:].rsplit('.',1)
+
+class bcolors:
+	HEADER 	 = '\033[95m'
+	OKBLUE 	 = '\033[94m'
+	OKCYAN 	 = '\033[96m'
+	OKGREEN  = '\033[92m'
+	WARNING  = '\033[93m'
+	FAIL 	 = '\033[91m'
+	ENDC 	 = '\033[0m'
+	BOLD 	 = '\033[1m'
+	UNDERLINE= '\033[4m'
+
+def RED(msg:str) -> str:
+	return f"{bcolors.FAIL}{msg}{bcolors.ENDC}"
+
+def GREEN(msg:str) -> str:
+	return f"{bcolors.OKGREEN}{msg}{bcolors.ENDC}"
+
+def BLUE(msg:str) -> str:
+	return f"{bcolors.OKCYAN}{msg}{bcolors.ENDC}"
+
+def HEADER(msg:str) -> str:
+	return f"{bcolors.HEADER}{msg}{bcolors.ENDC}"
+
+def debug(*args,**kwargs):
+	print(f'[{HEADER(__filename__)}]',*args,**kwargs)
+
+
+def create_cmd(
+	compiler:str,
+	project:Project) -> str:
+	
+	version  :str  = project.version.lower() if project.version else ""
+	optimized :bool = True if project.optimize == "on" else False
+
+	version_flag  :str  = f'-std={version}' if version != "" else ""
+	
+	cmd = f'{compiler} {version_flag}'
+
+	for file in project.srcfiles:
+		cmd += f' ./{file} '
+		
+
+	for folder in project.includedirs:
+		cmd += f' -I{folder} '
+
+	for folder in project.libdirs:
+		cmd += f' -L{folder} '
+
+	for warning in project.warnings:
+		cmd += f' -W{warning} '
+	
+	for define in project.defines:
+		cmd += f' -D{define} '
+
+	for file in project.libfiles:
+		cmd += f' -l{file} '
+		
+	
+	cmd += (" -O3 " if optimized else "-O0 ")
+	
+	cmd += f'-o{project.executable_path()}'
+
+	return cmd
+
+def __main__():
+	default_project_path = 'CyberXEngine.toml'
+	project_path = sys.argv[1] if len(sys.argv) > 1 else default_project_path
+	
+	with open(project_path, "rb") as f:
+		data	 	= defaultdict(lambda:"",toml.load(f))
+		project = Project(data)
+	
+	config = {
+		"compiler" : project.compiler if project is not None else 'g++', 
+	}
+
+	cmd = create_cmd(
+		compiler=config['compiler'],
+		project =project
+	)
+	
+	debug(f"INFO: Building project from file : {BLUE(project_path)}\n")
+	debug(f"INFO: Command Generated: {cmd}\n")
+	debug(f"INFO: Running project from file : {BLUE(project_path)}\n")
+	
+	code = subprocess.run(cmd)
+	
+	if code.returncode == 0:
+		debug(f'INFO: {GREEN("Compilation Succeded")} return code was zero, usually means success')
+		subprocess.run(project.executable_path())
+	else:
+		debug(f'ERROR: {RED("Compilation Failed")} return code was {RED("non-zero")}, usually means bad things')
+
+if __name__ == '__main__':
+	__main__()
